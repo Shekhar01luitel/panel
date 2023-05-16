ed7325ba46dcfc229188d9a4f01a00981805642d
shekhar-luitel-byte <124148571+Shekhar01luitel@users.noreply.github.com>
2023-05-16 17:12:07 +0545

    allcode
    first

---
+++ Newadmin/login.php
@@ -0,0 +1,102 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Retrieve the submitted form data
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // Check if the entered credentials match the expected values
+    if (($email === 'surendra')||($email === 'shekhar') && ($password === 'surendra@123')||($password === 'shekhar')) {
+        // Redirect the user to the home page or perform any other desired action
+        header('Location: home.html');
+        exit();
+        
+    } else {
+        // Credentials are incorrect, display an error message
+        $error = 'Incorrect username or password';
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MSS ERP | Log in</title>
+
+  <!-- Titlebar image -->
+  <link rel="icon" href="dist/img/fav.ico" type="image/x-icon">
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <!-- /.login-logo -->
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <p class="h1"><b>MSS </b>ERP</p>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="login.php" method="post">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Username" name="email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember" name="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+        <?php if (isset($error)) : ?>
+        <div class="alert alert-danger"><?php echo $error; ?></div>
+    <?php endif; ?>
+      </form>
+      <!-- /.card-body -->
+   </div>
+   <!-- /.card -->
+</div>
+<!-- /.login-box -->
+
+<footer>
+  <strong>Copyright &copy; <a href="https://garudacode.netlify.app/" target="_blank">Garud Code & Technology</a>.</strong>
+</footer>
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+</html>
+
